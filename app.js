91241291f5470fc0d8a79f67e7a570685337f545
david-mateogit <davidmateo@outlook.com>
2019-07-30 10:07:44 -0400

    initial deploy

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.use('/campgrounds', campgroundRoutes);
 app.use(indexRoutes);
 
 //START EXPRESS SERVER
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('The YelpCamp Server has started!');
 });
